dd39900c208698b5c86bb5d9f0cae87528087761
sabbirDIU-222 <57146729+sabbirDIU-222@users.noreply.github.com>
2020-02-03 00:40:38 +0600

    Add files via upload

---
+++ OopBasic.java
@@ -0,0 +1,23 @@
+
+package javaoop;
+
+public class OopBasic {
+    
+    String name,id;
+    long phon;
+    
+    
+    void display()
+    {
+        System.out.println("name : " + name);
+        System.out.println("name : " + id);
+        System.out.println("name : " + phon);
+        
+        
+        System.out.println("\n\n");
+        
+    }
+            
+    
+    
+}
---
+++ Teacher.java
@@ -0,0 +1,35 @@
+
+package javaoop;
+
+
+public class Teacher {
+    public static void main(String[] args) {
+        
+        OopBasic teacher1 = new OopBasic();
+        
+        teacher1.name = "shimul islam ";
+        teacher1.id = "fdff";
+        teacher1.phon = 3244243;
+        teacher1.display();
+        
+        
+       OopBasic teacher2 = new OopBasic();
+        
+        teacher2.id = "marif@gmail.com"; 
+        teacher2.phon = 12345;      
+        teacher2.name = "marif khan";
+        teacher2.display();
+       
+        
+
+        
+        
+        
+                
+        
+        
+        
+        
+    }
+    
+}
