01c3625ef73046e972f5eca1feb633c71e06fc09
sabbirDIU-222 <57146729+sabbirDIU-222@users.noreply.github.com>
2020-02-12 22:24:50 +0600

    Add files via upload

---
+++ ComplexNumber.java
@@ -0,0 +1,63 @@
+
+package online_shop;
+
+
+public class ComplexNumber {
+    
+    int real,image;
+    
+    ComplexNumber()
+    {
+        
+    }
+    
+    ComplexNumber(int real,int image)
+    {
+        this.image = image;
+        this.real = real;
+        
+    }
+    
+    void display()
+    {
+        System.out.println(real+ "+" + image+"i");
+    }
+    
+   
+    
+       ComplexNumber add(ComplexNumber obj,ComplexNumber obj1)
+    {
+        ComplexNumber obj3 = new ComplexNumber();
+        
+        obj3.real = obj.real + obj1.real;
+        obj3.image = obj.image + obj1.image;
+        
+        return obj3;
+        
+    }
+    
+    
+    
+    
+    public static void main(String[] args) {
+        
+        ComplexNumber obj = new ComplexNumber(5,9);
+        ComplexNumber obj1 = new ComplexNumber(10,4);
+        
+        obj.display();
+        obj1.display();
+        
+        ComplexNumber obj4 = new ComplexNumber();
+        
+        obj4 = obj4.add(obj, obj1);
+        obj4.display();
+        
+        
+        
+        
+    }
+    
+    
+    
+}
+ 
