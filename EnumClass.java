914e8489b18cca3049db0143664be2d4bee40609
sabbirDIU-222 <57146729+sabbirDIU-222@users.noreply.github.com>
2020-05-29 18:25:51 +0600

    Add files via upload

---
+++ EnumClass.java
@@ -0,0 +1,27 @@
+
+package javaalgorithm;
+
+
+public enum EnumClass {
+    //Every enum constant is always implicitly public static final. Since it is static, 
+    //we can access it by using enum Name. 
+    //Since it is final, we can’t create child enums.
+    
+    sabbir,samiwon;
+    
+}
+
+class Test{
+    public static void main(String[] args) {
+        EnumClass test= EnumClass.samiwon;
+        System.out.println(EnumClass.sabbir +  "  love "+ test);
+      
+    }
+    // we can not declare any enumeration in any method 
+    // we can call it from main method 
+    public void test()
+    {
+        EnumClass i = EnumClass.samiwon;
+        System.out.println(EnumClass.sabbir +  "  hate "+ i);
+    }
+}
